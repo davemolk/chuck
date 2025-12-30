fae692570717d5153fd5de9026625acfb797164a
Dave Molk <davemolk@gmail.com>
2025-12-29 19:58:12 -0800

    add middleware

--- internal/api/http/routes.go
+++ internal/api/http/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/davemolk/chuck/internal/api/http/handlers"
+	"github.com/davemolk/chuck/internal/api/http/middleware"
 	"github.com/davemolk/chuck/internal/service"
 	"go.uber.org/zap"
 )
@@ -14,12 +15,18 @@ type Services struct {
 
 func NewRoutes(logger *zap.Logger, services *Services) http.Handler {
 	mux := http.NewServeMux()
-	jokes := handlers.NewJokeHandlers(logger, services.JokeService)
+
 	health := handlers.NewHealthHandlers()
+	jokes := handlers.NewJokeHandlers(logger, services.JokeService)
 
 	mux.HandleFunc("GET /health", health.HealthCheck)
 	mux.HandleFunc("POST /api/v1/jokes/personalized", jokes.GetPersonalized)
 	mux.HandleFunc("GET /api/v1/jokes/random", jokes.GetRandom)
 
-	return mux
+	var handler http.Handler = mux
+	handler = middleware.Logger(logger)(handler)
+	handler = middleware.RequestID(handler)
+	handler = middleware.RecoverPanic(logger)(handler)
+
+	return handler
 }
